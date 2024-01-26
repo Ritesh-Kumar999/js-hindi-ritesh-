04cf4f043242408bb5974161e15a0ab4e03ec18d
Ritesh <143990447+Ritesh-Kumar999@users.noreply.github.com>
2024-01-26 11:31:07 +0000

    discussion on data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 14446
+let accountEmail='ritesh@google.com'
+var accountPassword="1234"
+accountCity="jaiout"
+/*
+ prefered not to use var
+ becasuse of issue in block scope and functional scope
+*/
---
+++ 01_basics/02dataTypes.js
@@ -0,0 +1,14 @@
+"use strict ";//treat all JS code as newer version 
+//alert(3+3)//it will give errror as we are using node.js not browser
+
+let name ="ritesh"
+let age =18
+let isLoggedIn=false
+//number => 2 to power 53
+//bigint
+//string =>""
+//undefined
+//symbol=>unique
+
+//object
+console.log(typeof age)
